5d2d40d4264dd1c0dde4128bedf26e9b8618f6ac
Juntakk <nick.habashi@gmail.com>
2025-09-07 18:48:52 -0400

    Refactor code structure for improved readability and maintainability

--- src/components/shared/UI.tsx
+++ src/components/shared/UI.tsx
@@ -14,8 +14,8 @@ import {
   IoMenu,
   IoClose,
 } from "react-icons/io5";
-import CV from "../../../public/assets/Nicolas_Gauthier.pdf";
-import CV_fr from "../../../public/assets/Nicolas_Gauthier_FR.pdf";
+import CV from "../../../public/assets/English.pdf";
+import CV_fr from "../../../public/assets/French.pdf";
 import { motion } from "framer-motion";
 import NavLink from "../ui/navlink";
 import { TbAwardFilled } from "react-icons/tb";
